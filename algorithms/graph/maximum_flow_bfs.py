b8af06a1205385c8b0b099056b856ff22ec66ebc
zsabbagh <zassabbagh@gmail.com>
2023-02-17 11:13:50 +0100

    Add branch coverage init. Refs #1
    Adds branch coverage for `maximum_flow_bfs.py`.

--- algorithms/graph/maximum_flow_bfs.py
+++ algorithms/graph/maximum_flow_bfs.py
@@ -33,7 +33,12 @@ def maximum_flow_bfs(adjacency_matrix):
     new_array = copy.deepcopy(adjacency_matrix)
     total = 0
 
+    branches = set()
+
     while True:
+
+        branches.add(1)
+
         #setting min to max_value
         min_flow = math.inf
         #save visited nodes
@@ -50,11 +55,18 @@ def maximum_flow_bfs(adjacency_matrix):
 
         #BFS to find path
         while bfs.qsize() > 0:
+
+            branches.add(2)
+
             #pop from queue
             src = bfs.get()
             for k in range(len(new_array)):
+
+                branches.add(3)
+
                 #checking capacity and visit
                 if(new_array[src][k] > 0 and visited[k] == 0 ):
+                    branches.add(4)
                     #if not, put into queue and chage to visit and save path
                     visited[k] = 1
                     bfs.put(k)
@@ -62,6 +74,7 @@ def maximum_flow_bfs(adjacency_matrix):
 
         #if there is no path from src to sink
         if visited[len(new_array) - 1] == 0:
+            branches.add(5)
             break
 
         #initial setting
@@ -69,8 +82,10 @@ def maximum_flow_bfs(adjacency_matrix):
 
         #Get minimum flow
         while tmp != 0:
+            branches.add(6)
             #find minimum flow
             if min_flow > new_array[path[tmp]][tmp]:
+                branches.add(7)
                 min_flow = new_array[path[tmp]][tmp]
             tmp = path[tmp]
 
@@ -81,7 +96,15 @@ def maximum_flow_bfs(adjacency_matrix):
         while tmp != 0:
             new_array[path[tmp]][tmp] = new_array[path[tmp]][tmp] - min_flow
             tmp = path[tmp]
+            branches.add(8)
 
         total = total + min_flow
+    
+    with open('data/branch-coverage', 'a') as f:
+        f.write('maximum_flow_bfs:' + str(len(branches) / 8.0))
+        for i in range(1, 9):
+            if i not in branches:
+                f.write(' ' + str(i))
+        f.write('\n')
 
     return total
---
+++ tests/__init__.py
@@ -0,0 +1,2 @@
+with open('data/branch-coverage', 'w') as f:
+    f.write('--- branch-coverage ---\n')
