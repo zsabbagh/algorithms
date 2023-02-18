fee73cacf3811d50233973488b22d97bb77152ed
zsabbagh <zassabbagh@gmail.com>
2023-02-18 12:58:18 +0100

    Implement tests for `RBTree`. Refs #3
    Increases branch coverage of entire RBTree class
    from 0% to 61%.

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -8,9 +8,13 @@ from algorithms.tree.traversal import (
 )
 from algorithms.tree.b_tree import BTree
 
+import random
+
 from algorithms.tree import construct_tree_postorder_preorder as ctpp
 
 from algorithms.tree.fenwick_tree.fenwick_tree import Fenwick_Tree
+
+from algorithms.tree.red_black_tree.red_black_tree import RBTree, RBNode
 
 import unittest
 
@@ -175,6 +179,130 @@ class TestFenwickTree(unittest.TestCase):
         ft.update_bit(bit_tree, 2, 11)
         self.assertEqual(23, ft.get_sum(bit_tree, 4))
 
+class TestRBTree(unittest.TestCase):
+    # Noticed that the RB tree probably is not 
+    # finished
+    def _populate_tree(self, ls: list, tree=None):
+        """
+            Inserts all values from list ls
+            in order of appearance
+        """
+        rbt = RBTree() if tree is None else tree
+        for val in ls:
+            node = RBNode(val, 0)
+            rbt.insert(node)
+        return rbt
+
+    def _randarr_randlen(self, abs_val: int, min_len: int, max_len: int):
+        """
+            Returns an array of random values in span
+            [-abs_val, abs_val] of a random length
+            between min_len and max_len
+        """
+        return [ random.randint(-abs_val, abs_val) for _ in range(random.randint(min_len, max_len)) ]
+
+    def _get_color_node(self, rbt, color=0, min_depth=0) -> RBNode:
+        """
+            Gets the first node with a color
+            Checks right child first, then left
+            Gets the first one of minimum depth min_depth
+        """
+        stack = [rbt.root]
+        depth = 0
+        while len(stack) > 0:
+            root = stack.pop()
+            if root.color == 1 and depth >= min_depth:
+                return root
+            depth += 1
+            if root.right is not None:
+                if root.right.color == 1 and depth >= min_depth:
+                    return root.right
+                stack.append(root.right)
+            if root.left is not None and min_depth:
+                if root.left.color == 1:
+                    return root.left
+                stack.append(root.left)
+        return None
+    
+    def _ensure_coloring(self, rbt):
+        """
+            A red node should have no
+            red children
+        """
+        if rbt.root is None:
+            return
+        self.assertEqual (rbt.root.color, 0)
+        stack = [rbt.root]
+        while len(stack) > 0:
+            root = stack.pop()
+            if root.right is not None:
+                stack.append(root.right)
+                # Equals to 2 means both are red
+                self.assertNotEqual (root.right.color+root.color, 2)
+            if root.left is not None:
+                stack.append(root.left)
+                self.assertNotEqual (root.left.color+root.color, 2)
+            if len(stack) < 1:
+                break
+
+    def test_insert(self):
+        # Only initialiser is None...
+        rbt = self._populate_tree([55, 40, 65, 60, 75, 57])
+        self.assertEqual (rbt.root.val, 55)
+        self.assertEqual (rbt.root.left.val, 40)
+        self.assertEqual (rbt.root.right.val, 65)
+        self.assertEqual (rbt.root.right.left.val, 60)
+        self.assertEqual (rbt.root.right.left.left.val, 57)
+        self.assertEqual (rbt.root.right.right.val, 75)
+        self._ensure_coloring(rbt)
+    
+    def test_delete_fixup(self):
+        # Only initialiser is None...
+        # Insert already tested
+        rbt = self._populate_tree([55, 40, 65, 60, 75, 57])
+        root = rbt.root.right.left
+        self.assertEqual (root.val, 60)
+        # Root should be black
+        self.assertEqual (root.color, 0)
+        rbt.delete_fixup(root)
+        # Coloring should be preserved
+        self._ensure_coloring(rbt)
+
+    def test_delete_fixup_random_tree(self):
+        """
+            Tests delete_fixup on random trees
+        """
+        # iterations of random testcases
+        col = 0
+        for _ in range(4):
+            rbt = self._populate_tree(self._randarr_randlen(50, 10, 20))
+            # delete a black node
+            node = self._get_color_node(rbt, color=col)
+            while node is not None:
+                rbt.delete_fixup(node)
+                # Coloring should be preserved
+                self._ensure_coloring(rbt)
+                if col == 0:
+                    col = 1
+                else:
+                    col = 0
+                node = self._get_color_node(rbt, color=col, min_depth=1)
+
+    def test_delete(self):
+        """
+            Tests delete
+        """
+        # iterations of random testcases
+        rbt = self._populate_tree([5, 2, 6])
+        self.assertEqual (rbt.root.val, 5)
+        self.assertEqual (rbt.root.right.val, 6)
+        self.assertEqual (rbt.root.left.val, 2)
+        rbt.delete(rbt.root.right)
+        rbt.delete(rbt.root.left)
+        self.assertTrue (rbt.root.right is None)
+        self.assertTrue (rbt.root.left is None)
+        self._populate_tree(range(10,20), tree=rbt)
+        self.assertEqual (rbt.maximum(rbt.root).val, 19)
 
 if __name__ == '__main__':
     unittest.main()
